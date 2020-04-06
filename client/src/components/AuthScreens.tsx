195fae74a63fb306867bd15f20445d3a69aa60c3
Radu Costica <radz1997c@gmail.com>
2020-04-06 18:34:11 +0100

    authentication setup

---
+++ client/src/components/AuthScreens.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthScreens: React.FC = () => {
+  return <div>Hello world</div>;
+};
+
+export default AuthScreens;
--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,41 +1,27 @@
 import React, { createContext, useReducer } from "react";
 import axios from "axios";
 
-interface Auth {
-  token: string | null;
-  isAuthenticated: boolean;
-  loading: boolean;
-  error: string | null;
-}
-
-interface Data {
-  email: string;
-  password: string;
-}
-
-const initialState: Auth = {
-  token: null,
-  isAuthenticated: false,
-  loading: true,
-  error: null,
-};
-
-type Actions = { type: "SIGN_IN"; payload: string };
+import { User, Auth, reducer } from "./AuthReducer";
 
 export const AuthContext = createContext<any>(null);
 
-const reducer = (state: Auth, action: Actions) => {
-  switch (action.type) {
-    case "SIGN_IN":
-      localStorage.setItem("token", action.payload.token);
-      return {};
-  }
-};
-
 const AuthProvider: React.FC = ({ children }) => {
+  const initialState: Auth = {
+    token: "",
+    isAuthenticated: false,
+    loading: true,
+    error: "",
+    user: {
+      email: "",
+      username: "",
+      github_user: null,
+      points: 0,
+      start_challenge: "",
+    },
+  };
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const login = async (data: Data) => {
+  const login = async (data: { email: string; password: string }) => {
     const config = {
       headers: {
         "Content-Type": "application/json",
@@ -45,13 +31,50 @@ const AuthProvider: React.FC = ({ children }) => {
       const res = await axios.post("/api/auth", data, config);
 
       console.log(res.data);
-      dispatch({ type: "SIGN_IN", payload: res.data });
+      dispatch({ type: "SIGN_IN", token: res.data });
     } catch (error) {
       console.log(error);
     }
   };
+
+  const register = async (data: {
+    email: string;
+    username: string;
+    password: string;
+    github_user?: string;
+  }) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/users", data, config);
+
+      dispatch({ type: "REGISTER", token: res.data });
+    } catch (error) {
+      return console.log(error);
+    }
+  };
+
+  const startChallenge = async (data: { date: string }) => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const res = await axios.post("/api/users/start", data, config);
+
+      dispatch({ type: "START_CHALLENGE", date: data.date });
+    } catch (error) {
+      return console.log(error);
+    }
+  };
   return (
-    <AuthContext.Provider value={{ login }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ login, register, startChallenge }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
